8809b043a74c6d1cd91ee4dcbcf3fa5d92f1d219
turbulent7 <arvkj04943@gmail.com>
2025-02-24 12:03:42 +0530

    updated soap_adapters

--- adapters/soap_adapters.py
+++ adapters/soap_adapters.py
@@ -1,7 +1,7 @@
 from zeep import Client
 
 def soap_to_json():
-    wsdl_url = "https://www.example.com/legacy.wsdl"  # Replace with actual WSDL URL
+    wsdl_url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL"  # Replace with actual WSDL URL
     client = Client(wsdl=wsdl_url)
     response = client.service.GetData()
     return {"data": response}
