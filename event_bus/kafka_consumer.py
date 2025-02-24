0628bc3e8978ac9b9cd2becc903621740f05acbc
turbulent7 <arvkj04943@gmail.com>
2025-02-24 11:35:28 +0530

    updated consumer

---
+++ event_bus/kafka_consumer.py
@@ -0,0 +1,13 @@
+from kafka import KafkaConsumer
+import json
+
+consumer = KafkaConsumer(
+    "processed_data",
+    bootstrap_servers="localhost:9092",
+    auto_offset_reset="earliest",
+    value_deserializer=lambda v: json.loads(v.decode("utf-8"))
+)
+
+print("Listening for processed data...")
+for message in consumer:
+    print("Received:", message.value)
